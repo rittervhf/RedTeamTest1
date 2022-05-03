f42c4f3c2c8574a1b05ce84c44e3f2a8610fcbbc
hugorv <hugorv@graphcore.ai>
2022-05-03 14:19:28 +0100

    Correction

--- loader.ps1
+++ loader.ps1
@@ -3,6 +3,7 @@ Write-Host 'Achieved via:'
 Write-Host 'T1566.001 - Spearphishing Attachment'
 Write-Host 'https://attack.mitre.org/techniques/T1566/001/''
 
+$Shell = New-Object -ComObject "WScript.Shell"
 $Button = $Shell.Popup("Click OK to continue.", 0, "Continue", 0)
 Write-Host 'Attempting stage 2 execution...'
 Write-Host 'Attempting stage 2 execution...'
