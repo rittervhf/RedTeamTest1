a107f94c1e4db8f4bf473b076677a59f871a9e45
hugorv <hugorv@graphcore.ai>
2022-05-05 11:14:47 +0100

    Fixed closing quotes

--- loader.ps1
+++ loader.ps1
@@ -1,9 +1,8 @@
 Write-Host 'Initial access successful!!!'
 Write-Host 'Achieved via:'
 Write-Host 'T1566.001 - Spearphishing Attachment'
-Write-Host 'https://attack.mitre.org/techniques/T1566/001/''
+Write-Host 'https://attack.mitre.org/techniques/T1566/001/'
 
 $Shell = New-Object -ComObject "WScript.Shell"
 $Button = $Shell.Popup("Click OK to continue.", 0, "Continue", 0)
-Write-Host 'Attempting stage 2 execution...'
 Write-Host 'Attempting stage 2 execution...'
